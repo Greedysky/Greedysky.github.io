fa168de808539ec6e91122db14228732d2808ba0
Greedysky <greedysky@163.com>
2020-09-19 14:02:16 +0800

    2020-09-19 13:59:57

--- ttkdocs/html/search-config.php
+++ ttkdocs/html/search-config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Sun Jul 12 2020 14:03:45 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
+  'logo' => "Generated on Sat Sep 19 2020 13:47:20 for TTKMusicplayer by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.8 ",
 );
 
 </script>
